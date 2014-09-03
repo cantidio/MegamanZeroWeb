e186c37962613528d7390914276aa76af260cdbb
cantidio <aniquilatorbloody@gmail.com>
2014-09-03 17:21:30 -0300

    small test.

--- bin/server.dart
+++ bin/server.dart
@@ -1,7 +1,12 @@
+import 'dart:io' show File, HttpServer, Platform;
 import 'package:start/start.dart';
 
 main() {
-  start(host: '0.0.0.0', port: 8080).then((Server app) {
+  var portEnv = Platform.environment['PORT'];
+  var port = portEnv == null ? 9999 : int.parse(portEnv);
+
+  print("port: $port");
+  start(host: '0.0.0.0', port: port).then((Server app) {
     app.static('../build/web');
   });
 }
